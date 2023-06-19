bf1dbea31acfd74bff795bb2ce66d0d426450b25
Happy2579 <81232763+Happy2579@users.noreply.github.com>
2023-06-19 19:11:26 +0530

    Delete Practice directory

--- Practice/myapplication/src/main/java/com/example/myapplication/MainActivity.java
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication;
-import java.util.*;
-
-public class MainActivity{
-    public static void main(String[] args){
-        System.out.println("My name is Harpreet Singh");
-    }
-
-        }
